6cad84152c37d72187e00b696018663cd3f62b88
iam-Mai <sasithorn.senmaithai@gmail.com>
2019-01-26 20:49:08 -0500

    Updates

---
+++ precache-manifest.e066d4c49b63f25d8cb2b3661fe6db76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3764da854687d86c49e1",
+    "url": "/iam-Mai/BENote-app.git/static/js/runtime~main.3764da85.js"
+  },
+  {
+    "revision": "82241ffb152be11dc245",
+    "url": "/iam-Mai/BENote-app.git/static/js/main.82241ffb.chunk.js"
+  },
+  {
+    "revision": "fce7f04e65d1cb1d14c8",
+    "url": "/iam-Mai/BENote-app.git/static/js/1.fce7f04e.chunk.js"
+  },
+  {
+    "revision": "82241ffb152be11dc245",
+    "url": "/iam-Mai/BENote-app.git/static/css/main.d7d1c535.chunk.css"
+  },
+  {
+    "revision": "fce7f04e65d1cb1d14c8",
+    "url": "/iam-Mai/BENote-app.git/static/css/1.0ae6b27f.chunk.css"
+  },
+  {
+    "revision": "ba16a144a96f3dbaec6a607dc38fb5a7",
+    "url": "/iam-Mai/BENote-app.git/index.html"
+  }
+];
